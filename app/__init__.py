60afa9df37e7fb5f1eb67f4620f0b0e6d6d0bfa9
charles-os <charonkobarix@yahoo.com>
2019-12-02 00:14:42 +0300

    fix a bug

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ def create_app(config_name):
 
 
     # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+    app.config.from_object(Config_options[config_name])
 
     # Initializing flask extensions
     bootstrap.init_app(app)
