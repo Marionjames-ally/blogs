889ec5c1afc1ed619169da52ff25c2dc5de8aee0
charles-os <charonkobarix@yahoo.com>
2019-12-02 00:12:53 +0300

    add authendication and user,blog model

--- app/user.py
+++ app/user.py
@@ -1,3 +1,8 @@
+from flask_login import UserMixin, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from . import db, login_manager
+
 class Quote:
     '''
     quote class to define quote Objects
@@ -8,34 +13,39 @@ class Quote:
         self.author = author
         self.quote = quote
         self.permalink = permalink
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
 
-class Review:
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
+    image = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(70), nullable=False)
+    blogs = db.relationship('Blog', backref='author', lazy="dynamic")
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    pass_secure = db.Column(db.String(255))
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-    all_reviews = []
-
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
-
-
-    def save_review(self):
-        Review.all_reviews.append(self)
-
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
-
-    @classmethod
-    def get_reviews(cls,id):
-
-        response = []
-
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
-
-        return response 
+    @password.setter
+    def password(self, password):
+		
+		def verify_password(self, password):
+			return check_password_hash(self.pass_secure, password)
+			
+			def __repr__(self):
+				return f'User {self.username}', '{self.image}'
+    
+class Blog(db.Model):
+	 __tablename__ = 'blogs'
+	 id = db.Column(db.Integer, primary_key=True)
+	 title = db.Column(db.String(110), nullable=False)
+	 content = db.Column(db.Text, nullable=False)
+	 user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+	 comment = db.relationship('Comment', backref='pitch', lazy='dynamic') 
--- config.py
+++ config.py
@@ -17,3 +17,4 @@ class DevConfig(Config):
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
+}
