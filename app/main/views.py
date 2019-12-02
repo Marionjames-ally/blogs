5f1e53c11ce84dc872681c00a2158acefe036b4f
charles-os <charonkobarix@yahoo.com>
2019-12-02 13:32:36 +0300

    add update profile function

--- app/main/views.py
+++ app/main/views.py
@@ -50,3 +50,55 @@ def delete_blog(id):
   
     return redirect(url_for('main.index',))
 
+
+@main.route('/blog/update, <int:id>',methods=['GET','POST'])
+@login_required
+def update_blog(id):
+    blog=Blog.query.filter_by(id=id).first()
+    if blog is None:
+        return(404)
+    form = UpForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        print(current_user._get_current_object().id)
+        blog = Blog(user_id=current_user._get_current_object().id,
+                title=title, content=content)
+        
+        db.session.add(blog)
+        db.session.commit()
+    
+        return redirect(url_for('main.index', blog_id=blog.id))
+    elif request.method == 'GET':
+        title = form.title.data
+        content = form.content.data
+    return render_template('blog.html',form=form)
+
+@main.route('/user/<uname>')
+@login_required
+def profile(uname):
+    user = User.query.filter_by(username=uname).first()
+
+    if user is None:
+        return(404)
+    return render_template('profile,profile.html', user=user)
+
+
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        return(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form=form)
