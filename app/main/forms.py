7ee12b63d8686197e4f58f0b435104edfcbc2479
charles-os <charonkobarix@yahoo.com>
2019-12-02 14:20:01 +0300

    create forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import Required
 
+
 class BlogForm(FlaskForm):
-    title= StringField('Title',validators=[Required()])
-    content = TextAreaField('pitch', validators=[Required()])
+    title = StringField('Title', validators=[Required()])
+    content = TextAreaField('quote', validators=[Required()])
+
+    submit = SubmitField('Submit')
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+    content = TextAreaField('Add comment', validators=[Required()])
+    submit = SubmitField()
+    
+class UpForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    content = TextAreaField('quote', validators=[Required()])
 
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,32 +1,32 @@
-from flask import render_template,request,redirect,url_for
-from . import main
-from app.request import get_quote
+from flask import abort, redirect, render_template, request, url_for,abort,flash
 from flask_login import current_user, login_required, login_user, logout_user
-from .. import db, photos
+
+from app.request import get_quote
+
+from .. import db
 from ..user import Blog, Comment, User
 from . import main
-from .forms import BlogForm,commentForm, UpdateProfile, UpForm
+from .forms import BlogForm, CommentForm, UpdateProfile,UpForm
 
-@main.route('/')
+
+@main.route('/', methods=['GET', 'POST'])
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
+    title = 'my blogquote'
+    quote = get_quote()
+    blogs = Blog.query.all()
+    return render_template('index.html', title=title, quote=quote, blogs=blogs)
 
-    title ='my blogquote'
-    quote=get_quote()
-    blogs=Blog.querry.all
-    return render_template('index.html',title=title,quote=quote,blogs=blogs)
 
-
-@main.route('/blog/new',methods=['GET','POST'])
+@main.route('/blog/new', methods=['GET', 'POST'])
 @login_required
 def blogs():
-    '''
-    view blog function to create an new blog
-    '''
-    blog_form=BlogForm()
+    """
+    view blog function to create a new blog
+    """
+    blog_form = BlogForm()
 
     if blog_form.validate_on_submit():
         title = blog_form.title.data
@@ -39,9 +39,11 @@ def blogs():
         db.session.commit()
 
         return redirect(url_for('main.index'))
-        return render_template('new_blog.html', blog_form=blog_form)
 
-@main.route('/delete/blog, <int:id>',methods=['GET','POST'])
+    return render_template('new_blog.html',  blog_form=blog_form)
+
+
+@main.route('/delete/blog,<int:id>', methods=['GET', 'POST'])
 @login_required
 def delete_blog(id):
     blog = Blog.query.filter_by(id=id).first()
@@ -50,13 +52,12 @@ def delete_blog(id):
   
     return redirect(url_for('main.index',))
 
-
-@main.route('/blog/update, <int:id>',methods=['GET','POST'])
+@main.route("/blog/update/<int:id>", methods=['GET', 'POST'])
 @login_required
 def update_blog(id):
-    blog=Blog.query.filter_by(id=id).first()
+    blog = Blog.query.filter_by(id = id).first()
     if blog is None:
-        return(404)
+        abort(404)
     form = UpForm()
     if form.validate_on_submit():
         title = form.title.data
@@ -74,14 +75,22 @@ def update_blog(id):
         content = form.content.data
     return render_template('blog.html',form=form)
 
+#@main.route('/blog/<blog_id>')
+#def bloging(blog_id):
+   # blog = Blog.query.get_or_404(blog_id)
+   # return render_template('blog.html', title=blog)
+
+
+
 @main.route('/user/<uname>')
 @login_required
 def profile(uname):
     user = User.query.filter_by(username=uname).first()
 
     if user is None:
-        return(404)
-    return render_template('profile,profile.html', user=user)
+        abort(404)
+
+    return render_template("profile/profile.html", user=user)
 
 
 @main.route('/user/<uname>/update', methods=['GET', 'POST'])
@@ -89,7 +98,7 @@ def profile(uname):
 def update_profile(uname):
     user = User.query.filter_by(username=uname).first()
     if user is None:
-        return(404)
+        abort(404)
 
     form = UpdateProfile()
 
@@ -106,11 +115,35 @@ def update_profile(uname):
 
 @main.route('/user/<uname>/update/pic', methods=['POST'])
 @login_required
-def update_pic(uname):
-    user= User.query.filter_by(username=uname).first()
-    if 'photo' in request.files:
-        filename= photos.save(request.file['photo'])
-        path=f'photos/{filename}'
-        user.profile_pic_path = path
-        db.session_commit()
-        return redirect(url_for('main.profile', uname=uname))
+
+
+@main.route('/comment/new/<int:blog_id>', methods=['GET', 'POST'])
+@login_required
+def new_comment(blog_id):
+    form = CommentForm()
+    blog = Blog.query.get(blog_id)
+    if form.validate_on_submit():
+        content = form.content.data
+
+        new_comment = Comment(
+            content=content, user_id=current_user._get_current_object().id, blog_id=blog_id)
+        db.session.add(new_comment)
+        db.session.commit()
+
+        return redirect(url_for('.new_comment', blog_id=blog_id))
+
+    comments = Comment.query.filter_by(blog_id=blog_id).all()
+    print(comments)
+    return render_template('comments.html', form=form, blog=blog, comments=comments)
+
+
+@main.route('/delete/new/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_comment(id):
+    comment = Comment.query.filter_by(id=id).first()
+    form = CommentForm()
+    if comment is not None:
+        comment.delete_comment()
+        return redirect(url_for('main.index'))
+
+    return render_template('comments.html', form=form)
